f0ade679ad30383f553a936f71a29721f15ed884
Thirumoorthi123 <thirumoorthi1vp@gmail.com>
2020-01-22 17:04:44 +0530

    updated on 22 jan

--- web/myapp/routes/index.js
+++ web/myapp/routes/index.js
@@ -3,64 +3,65 @@ var router = express.Router();
 const { check, validationResult } = require('express-validator');
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
+var bodyParser = require("body-parser");
 var url = "mongodb://localhost:27017/";
 
 /* GET home page. */
-router.get('/',function(req,res){
-  MongoClient.connect(url, function(err, db) {
+router.get('/', function (req, res) {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     let dbo = db.db("portfolio");
     let d = new Date();
     // get the projects
-    dbo.collection('projects').find({}).limit(3).toArray(function(err, projects){
+    dbo.collection('projects').find({}).limit(3).toArray(function (err, projects) {
       if (err) throw err;
       console.log(JSON.stringify(projects));
-    
-// get the posts
-      dbo.collection('post').find({}).limit(3).toArray(function(err, post){
+
+      // get the posts
+      dbo.collection('post').find({}).limit(3).toArray(function (err, post) {
         if (err) throw err;
         console.log(JSON.stringify(post));
-      db.close();
-      res.render('index', { title: 'Portfolio | Mr.Thirumoorthi', 'indexNav': true, projects: projects, post: post});
+        db.close();
+        res.render('index', { title: 'Portfolio | Mr.Thirumoorthi', 'indexNav': true, projects: projects, post: post });
+      })
     })
-  })
-  });   
- 
+  });
+
 })
 /* GET project page with data*/
-router.get('/projects', function(req, res, next) {
-  MongoClient.connect(url, function(err, db) {
+router.get('/projects', function (req, res, next) {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     let dbo = db.db("portfolio");
     let d = new Date();
-    dbo.collection('projects').find().toArray(function(err, data){
+    dbo.collection('projects').find().toArray(function (err, data) {
       if (err) throw err;
 
       console.log(JSON.stringify(data));
       db.close();
-      res.render('projects', { title: 'Projects | Mr.Thirumoorthi', 'projectsNav': true, projects: data});
+      res.render('projects', { title: 'Projects | Mr.Thirumoorthi', 'projectsNav': true, projects: data });
     })
   });
-  
+
 });
-router.get('/projects/:id', function(req, res){
+router.get('/projects/:id', function (req, res) {
   let id = req.params.id;
-  console.log("myid",id);
-  console.log('id --- > ', typeof id); 
+  console.log("myid", id);
+  console.log('id --- > ', typeof id);
   //  once you got the project id
   // make the database call to check if it exists
-  MongoClient.connect(url, function(err, db) {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     let dbo = db.db("portfolio");
-    console.log("myid2",id);
-    
-    dbo.collection('projects').findOne({"_id":ObjectId(id)},function(err, data){
+    console.log("myid2", id);
+
+    dbo.collection('projects').findOne({ "_id": ObjectId(id) }, function (err, data) {
       if (err) throw err;
-      console.log("hii",data);
+      console.log("hii", data);
       db.close();
-      res.render('project-detail', { title: 'Projects | Mr.Thirumoorthi', 'projectsNav': true, data: data});
-    }) 
-  });  
+      res.render('project-detail', { title: 'Projects | Mr.Thirumoorthi', 'projectsNav': true, data: data });
+    })
+  });
   // if(id < data.length ){
   //   res.render('project-detail', { data : data[id] })
   // }else{
@@ -70,61 +71,109 @@ router.get('/projects/:id', function(req, res){
   // }
 })
 /* GET blog page with data*/
-router.get('/blog', function(req, res, next) {
-  MongoClient.connect(url, function(err, db) {
+router.get('/blog', function (req, res, next) {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     let dbo = db.db("portfolio");
     let d = new Date();
-    dbo.collection('post').find().toArray(function(err, post){
+    dbo.collection('post').find().toArray(function (err, post) {
       if (err) throw err;
       console.log(JSON.stringify(post));
       db.close();
-      res.render('blog', { title: 'Blog | Mr.Thirumoorthi', 'blogNav': true, post: post});
+      res.render('blog', { title: 'Blog | Mr.Thirumoorthi', 'blogNav': true, post: post });
     })
   });
 });
+
+/* start: GET Blog detail page section*/
+router.get('/blog/:id', function (req, res) {
+  let id = req.params.id;
+  console.log("myid", typeof id);
+  console.log('id --- > ', id);
+  //  once you got the project id
+  // make the database call to check if it exists
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    let dbo = db.db("portfolio");
+    console.log("myid2", id);
+    dbo.collection('post').findOne({ "_id": ObjectId(id) }, function (err, post) {
+      if (err) throw err;
+      console.log(JSON.stringify(post));
+      db.close();
+      res.render('blog-detail', { post: post })
+    });
+  });
+})
+
+/* End: GET Blog detail page section */
+
+/* Start:New letter page */
+router.get('/subscribe', function (req, res, next) {
+  res.render('index');
+  console.log('accessing post');
+});
+router.post('/subscribe', function (req, res) {
+  let email = req.body.email;
+  console.log("email");
+  console.log('accessing post');
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    let dbo = db.db("portfolio");
+    let data = { email };
+    dbo.collection('newsletter').insertOne(data, function (err, obj) {
+      if (err) throw err;
+      console.log(JSON.stringify(data));
+      db.close();
+      res.render('index', { success: true });
+    })
+  });
+});
+
+/* End : News letter page */
+
+
 /* GET About page.*/
-router.get('/about', function(req, res){
-  res.render('about', {title: 'About | Mr.Thirumoorthi', 'aboutNav': true});
+router.get('/about', function (req, res) {
+  res.render('about', { title: 'About | Mr.Thirumoorthi', 'aboutNav': true });
 })
 /* GET Contact page.*/
-router.get('/contact', function(req, res){
-  res.render('contact', {title: 'Contact | Mr.Thirumoorthi', 'contactNav': true});
+router.get('/contact', function (req, res) {
+  res.render('contact', { title: 'Contact | Mr.Thirumoorthi', 'contactNav': true });
 })
 
 router.post('/contact', [
-    check('email').isEmail().withMessage('Please enter a valid email id'),
-    check('mobile').isLength({ min: 10 }).withMessage('Mobile  number must be atleast 10 characters')
-  ],
-  function(req, res){
+  check('email').isEmail().withMessage('Please enter a valid email id'),
+  check('mobile').isLength({ min: 10 }).withMessage('Mobile  number must be atleast 10 characters')
+],
+  function (req, res) {
     const errors = validationResult(req);
     console.log(JSON.stringify(errors))
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
       var messages = [];
-      errors.errors.forEach(function(err){
+      errors.errors.forEach(function (err) {
         console.log(JSON.stringify(err))
         messages.push(err.msg)
       })
-      
-      res.render('contact', {errors: true, messages: messages, name, mobile, email, description});
-    }else{
+
+      res.render('contact', { errors: true, messages: messages, name, mobile, email, description });
+    } else {
       // read the values and save it in the DB
       let name = req.body.name;
       let mobile = req.body.mobile;
       let email = req.body.email;
       let description = req.body.description;
 
-      MongoClient.connect(url,function(err,db){
-        if(err)throw err;
+      MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
         var dbo = db.db('portfolio');
-        let contact = {name,mobile,email,description};
-        dbo.collection('contact').insertOne(contact, function(err,contactObj){
-          if(err)throw err;
-            console.log("one document instered. Id ="+contactObj._id);
-            db.close();
-        })   
-      });     
-      res.render('contact', {success: true});
+        let contact = { name, mobile, email, description };
+        dbo.collection('contact').insertOne(contact, function (err, contactObj) {
+          if (err) throw err;
+          console.log("one document instered. Id =" + contactObj._id);
+          db.close();
+        })
+      });
+      res.render('contact', { success: true });
     }
-})
+  })
 module.exports = router;
